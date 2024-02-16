140e6b9069964d17bc66069dbc51ae84f2ca4123
Americanmemes <mkarsten5014@student.egcc.edu>
2024-02-16 18:09:16 +0000

    Pending changes exported from your codespace

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ const Home: NextPage = (props) => {
   return (
     <div>
       <Head>
-        <title>Solana Scaffold</title>
+        <title>Purple Squirrel Faucet</title>
         <meta
           name="description"
-          content="Solana Scaffold"
+          content="Purple Squirrel Faucet"
         />
       </Head>
       <HomeView />
